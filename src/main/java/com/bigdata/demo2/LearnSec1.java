4029f2f9044dd2640471aea41dc207a6fa671207
boxiuhua <boxiuhua@gmail.com>
2025-11-07 23:59:07 +0800

    第3天学习-socket

---
+++ src/main/java/com/bigdata/demo2/LearnSec1.java
@@ -0,0 +1,40 @@
+package com.bigdata.demo2;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
+import org.apache.flink.core.fs.Path;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+
+/**
+ * socket测试
+ */
+public class LearnSec1 {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        DataStreamSource<String> dataStreamSource = env.socketTextStream("localhost", 8888);
+        System.out.println(dataStreamSource.getParallelism());
+        dataStreamSource.flatMap(new FlatMapFunction<String, Tuple2<String,Integer>>() {
+            @Override
+            public void flatMap(String line, Collector<Tuple2<String, Integer>> out) throws Exception {
+                String[] arr = line.split("\\s+");
+                for (String word:arr){
+                    out.collect(Tuple2.of(word,1));
+                }
+            }
+        }).keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+            @Override
+            public String getKey(Tuple2<String, Integer> value) throws Exception {
+                return value.f0;
+            }
+        }).sum(1).print();
+        env.execute();
+    }
+}
