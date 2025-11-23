be89543ef9070b399647375b15a8f42f7e78899f
boxiuhua <boxiuhua@gmail.com>
2025-11-23 11:37:41 +0800

    第7天学习-使用Watermark + AllowedLateness + SideOutput ,即使用侧道输出机制来单独收集延迟/迟到/乱序严重的数据,避免数据丢失!

---
+++ src/main/java/com/bigdata/demo3/Students.java
@@ -0,0 +1,15 @@
+package com.bigdata.demo3;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Students {
+    private int id;
+    private String name;
+    private int age;
+
+}
